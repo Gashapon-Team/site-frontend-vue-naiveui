fdb2e5097bf391cb93f42ccc30b303a660caa659
wico <wico.wen@neweb.com.tw>
2023-07-18 14:20:09 +0800

    feat: trying add input component for demo

---
+++ src/views/ComponentsDemoView.vue
@@ -0,0 +1,16 @@
+<template>
+  <n-space vertical>
+    <n-input maxlength="30" show-count clearable />
+    <n-input default-value="Yes" show-count clearable>
+      <template #count="{ value }">
+        {{ value.includes('Yes') ? '99+' : value.length }}
+      </template>
+    </n-input>
+    <n-input type="textarea" maxlength="30" show-count />
+    <n-input type="textarea" default-value="What?" show-count>
+      <template #count="{ value }">
+        {{ value.includes('What?') ? '99+' : value.length }}
+      </template>
+    </n-input>
+  </n-space>
+</template>
